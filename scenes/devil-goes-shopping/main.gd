6ae02cce67aad1e626f65bd4b619765562d6c8ec
Mifia <xzx._@outlook.com>
2024-10-06 09:25:29 +0300

    Remade counter scene to "devil goes shopping" (#3)
    - Added start devil cutscene (like in dumb ways to die)
    - Added devil to main scene as main character
    - Items now actually appear on conveyor
    
    
    ![image](https://github.com/user-attachments/assets/15df3a03-1f83-454f-879b-d564f45558a5)

---
+++ scenes/devil-goes-shopping/main.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@export var scene_list: Array[PackedScene] = []
+
+var difficulty = 1
+var is_fast: bool = false
+var time_to_complete = 10
+var spawn_amount = 3 # Так же сам дисплей ускоряется, который показывает эти штуки
+var memorize = 1
+var speed = 170.0
+
+func _ready() -> void:
+	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_MAILBOX)
+	var excluded_scene = "counter_item.tscn"
+	
+	var distance = 0
+	var previous_item
+	for i in range(spawn_amount):
+		var item = scene_list[randi() % scene_list.size()].instantiate()
+		$Items.add_child(item)
+		item.position = $Spawnpoint.position
+		if previous_item:
+			distance += previous_item.get_node("Sprite2D").texture.get_size().x + (randi() % 900)
+		item.position.x += distance
+		item.position.y += -70 + (randi() % 180)
+		previous_item = item
+	$Parallax2D.autoscroll.x = -speed
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	for item in $Items.get_children():
+		item.position.x -= speed * delta
