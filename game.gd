2afde471f53814bcf2dc308e65c158936ec27f9b
Mifia <xzx._@outlook.com>
2024-11-06 23:57:02 +0300

    Update main sprites and add animations on next() (#10)
    ![image](https://github.com/user-attachments/assets/8d61b929-a93c-4a0c-857a-2e96d2dc5a65)

--- game.gd
+++ game.gd
@@ -13,11 +13,20 @@ func _process(delta: float) -> void:
 	pass
 
 func next():
-	$Tombstones.next()
+	var anim: AnimationPlayer = $AnimationPlayer
+	anim.play("next")
+	#$Tombstones.next()
 	difficulty+=1
 
+func _tween_stuff():
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.parallel().tween_property($Tombstones,"position",Vector2($Tombstones.position.x-800,$Tombstones.position.y),0.6)
+	tween.parallel().tween_property($Ground,"scroll_offset",Vector2($Ground.scroll_offset.x-800,$Ground.scroll_offset.y),0.6)
+	tween.parallel().tween_property($Deco,"scroll_offset",Vector2($Deco.scroll_offset.x-180,$Deco.scroll_offset.y),0.6)
+	tween.parallel().tween_property($Deco2,"scroll_offset",Vector2($Deco2.scroll_offset.x-70,$Deco2.scroll_offset.y),0.6)
+	tween.parallel().tween_property($Moon,"scroll_offset",Vector2($Moon.scroll_offset.x-30,$Moon.scroll_offset.y),0.6)
 func _on_difficulty_changed(val):
-	print("diff increased")
 	%RichTextLabel.visible = true
 	difficulty = int(val)
 	%RichTextLabel.text = str(difficulty)
